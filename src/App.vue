ed8ce3a7e6670b8fba54d2fedba6a35c9759d3e2
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2023-02-15 20:00:59 +0100

    add basic functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,71 @@
 <template>
   <main class="compressor">
-    <button class="compressor__upload-button" @click="onUploadButtonClick">
-      Upload files
-      <br />
-      to compress
+    <div class="compressor__buttons">
+      <button class="compressor__upload-button" @click="onUploadButtonClick">
+        Upload
+        <input
+          multiple
+          type="file"
+          accept="image/*"
+          ref="uploadInput"
+          class="compressor__upload-input"
+          @change="onInputChange"
+        />
+      </button>
+      <button
+        class="compressor__upload-button compressor__upload-button--compress"
+        @click="compressFiles"
+      >
+        Compress
+      </button>
+      <button
+        class="compressor__upload-button compressor__upload-button--download"
+        @click="downloadFiles"
+      >
+        Download
+      </button>
+      <button
+        class="compressor__upload-button compressor__upload-button--clear"
+        @click="clearFiles"
+      >
+        Clear memory
+      </button>
+    </div>
+    <div class="compressor__rate">
+      Compression rate:
       <input
-        ref="uploadInput"
-        type="file"
-        class="compressor__input"
-        multiple
-        @change="onInputChange"
+        name="rate"
+        v-model="compressionRate"
+        type="number"
+        max="100"
+        min="0"
+        class="compressor__rate"
       />
-    </button>
+      %
+    </div>
+    <div>
+      <h1>Uploaded files: {{ uploadedAmount }}</h1>
+      <h1>Compressed files: {{ compressedAmount }}</h1>
+      <h1>READY TO DOWNLOAD: {{ isReadyToDownload ? "✅" : "❌" }}</h1>
+    </div>
   </main>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import Compressor from "compressorjs";
 
+const minifiedImages = ref([]);
 const uploadInput = ref();
+const uploadedAmount = ref(0);
+const compressedAmount = ref(0);
+const compressionRate = ref(100);
+const isCompressionLoading = ref(false);
+
+const isReadyToDownload = computed(() => {
+  const difference = uploadedAmount.value - compressedAmount.value;
+  return !difference && !!uploadedAmount.value;
+});
 
 function onUploadButtonClick() {
   uploadInput.value.click();
@@ -26,36 +73,111 @@ function onUploadButtonClick() {
 
 function onInputChange() {
   const images = Array.from(uploadInput.value.files);
-  images.forEach((image) => {
-    console.log(image);
+  uploadedAmount.value += images.length;
+}
+
+async function compressFiles() {
+  if (isCompressionLoading.value || isReadyToDownload.value) return;
+  isCompressionLoading.value = true;
+  const images = Array.from(uploadInput.value.files);
+  images.forEach((file) => {
+    new Compressor(file, {
+      quality: compressionRate.value / 100,
+      success(result) {
+        const file = new File([result], `min-${result.name}`);
+        minifiedImages.value.push(file);
+        compressedAmount.value++;
+        if (compressedAmount.value === uploadedAmount.value) {
+          console.log("FINISHED");
+          images.value = null;
+        }
+      },
+      error(e) {
+        console.warn(e);
+      },
+    });
   });
+  isCompressionLoading.value = false;
+}
+
+function downloadFiles() {
+  console.log(minifiedImages.value);
+}
+
+function clearFiles() {
+  minifiedImages.value = [];
+  uploadedAmount.value = 0;
+  compressedAmount.value = 0;
+  console.log("CLEARED");
 }
 </script>
 
 <style lang="scss" scoped>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
 .compressor {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 60px;
   height: 100vh;
+
+  &__buttons {
+    display: flex;
+    gap: 60px;
+  }
 
   &__upload-button {
     font-size: 25px;
     padding: 15px;
-    height: 220px;
-    width: 220px;
-    border: 5px solid rgb(41, 98, 168);
+    height: 180px;
+    width: 180px;
     border-radius: 100%;
-    background-color: rgb(235, 241, 246);
     cursor: pointer;
     font-weight: bold;
     transition: 100ms ease;
+    border: 5px solid rgb(41, 98, 168);
+    background-color: rgb(235, 241, 246);
+
     &:hover {
       background-color: rgb(219, 231, 241);
     }
+
+    &--compress {
+      border: 5px solid rgb(41, 168, 71);
+      background-color: rgb(235, 246, 236);
+
+      &:hover {
+        background-color: rgb(220, 241, 219);
+      }
+    }
+
+    &--download {
+      border: 5px solid rgb(168, 155, 41);
+      background-color: rgb(246, 245, 235);
+
+      &:hover {
+        background-color: rgb(239, 241, 219);
+      }
+    }
+
+    &--clear {
+      border: 5px solid rgb(168, 41, 41);
+      background-color: rgb(246, 235, 235);
+
+      &:hover {
+        background-color: rgb(241, 219, 219);
+      }
+    }
   }
 
-  &__input {
+  &__rate {
+    font-size: 35px;
+  }
+
+  &__upload-input {
     display: none;
   }
 }
